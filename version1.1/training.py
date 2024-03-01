88c03ae4cb9d7b6deb6a9677abefe499251816b1
Andreas Lie Massey <107505110+AndrlmMass@users.noreply.github.com>
2024-03-01 12:23:49 +0100

    small patch

--- version1.1/training.py
+++ version1.1/training.py
@@ -1,7 +1,23 @@
 # Loop over each neuron and update membrane potential, GABA, NMDA & AMPA, and weights
 
-def training_network(items, timesteps, dt, Vm, StimE_Ws, EE_EI_Ws, II_IE_Ws):
-    '''
+
+def training_network(
+    time,
+    dt,
+    tau_m,
+    Vm,
+    StimE_Ws,
+    EE_EI_Ws,
+    II_IE_Ws,
+    neuronal_layers,
+    g_ampa,
+    g_nmda,
+    g_gaba,
+    tau_nmda,
+    tau_ampa,
+    tau_gaba,
+):
+    """
     Variables explanation:
     'items' are each datapoint with its respective dimensions (variables)
     'timesteps' are the number of time units it takes to encode an item
@@ -11,36 +27,43 @@ def training_network(items, timesteps, dt, Vm, StimE_Ws, EE_EI_Ws, II_IE_Ws):
     'EE_EI_Ws' are the weights between excitatory to excitatory and between excitatory to inhibitory neurons
     'II_IE_Ws' are the weights between inhibitory to inhibitory and between inhibitory to excitatory neurons
 
-    '''
+    """
 
-    # Define length of looping
+    # Loop through
 
-    for j in range(items.shape):
-        for u in range(timesteps):
+    for j in range(1, time.shape):
+        # EXCITATORY NEURONS
+
+        # Update conductance
+        g_ampa[j] = g_ampa[j - 1] * dt / tau_ampa
+        g_nmda[j] = g_nmda[j - 1] * dt / tau_nmda
+        g_gaba[j] = g_gaba[j - 1] * dt / tau_gaba
+
+        # INHIBITORY NEURONS
+
+        for layer in range(neuronal_layers):
+
             # Loop over StimE weights
             for t in range(StimE_Ws):
-                # Calculate membrane potential
 
-                # Update spike-trace and spikes 
-
+                # Update spike-trace and spikes
+                d = 2
                 # Update weights using hetero -and homostasis based learning rules
 
-            # Loop over EE & EI weights 
+            # Loop over EE & EI weights
             for l in range(EE_EI_Ws):
                 # Calculate membrane potential
+                d = 1
+                # Update spike-trace and spikes
 
-                # Update spike-trace and spikes 
-                
                 # Update weights using hetero -and homostasis based learning rules
 
-            # Loop over II & IE weights 
+            # Loop over II & IE weights
             for p in range(II_IE_Ws):
                 # Calculate membrane potential
 
-                # Update spike-trace and spikes 
-                
+                # Update spike-trace and spikes
+                r = 2
                 # Update weights using hetero -and homostasis based learning rules
-        
-        # Implement sleep or some other homeostatic normalization method
 
-        
+    # Implement sleep or some other homeostatic normalization method
