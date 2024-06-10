a140cb237d8e496a5835a941604183f0547a6b4d
Andreas Lie Massey <107505110+AndrlmMass@users.noreply.github.com>
2024-06-10 09:54:20 +0200

    added conductance based membrane potential

--- version1.0/train_packages/membrane_potential.py
+++ version1.0/train_packages/membrane_potential.py
@@ -73,3 +73,48 @@ def update_membrane_potential(
     MemPot[N_excit_neurons:] = MemPot[N_excit_neurons:] - np.round(delta_MemPot_i, 4)
 
     return MemPot, I_in_i, I_in_e
+
+def update_membrane_potential_conduct(
+                    U,
+                    U_inh,
+                    U_exc,
+                    W_se,
+                    W_ee,
+                    W_ie,
+                    W_ei,
+                    S,
+                    dt,
+                    N_excit_neurons,
+                    N_input_neurons,
+                    N_inhib_neurons,
+                    V_rest,
+                    tau_m,
+                    alpha,
+                    tau_ampa,
+                    tau_nmda,
+                    tau_gaba,
+                    tau_d,
+                    tau_f,
+                    tau_a,
+                    delta_a,
+                    ):
+    
+    # Update components for membrane change
+    S_j = S[:] # Add post_synaptic neuron spikes here -> sure which are post in this scenario since we are going through all the neurons?
+    
+    x = dt * (((1-x)/tau_d) - u*x*S_j)
+    u = dt * (((U-u)/tau_f)+U*(1-u)*S_j)
+    
+    g_ampa = dt * ((-g_ampa/tau_ampa) + (np.sum(x*u*w*S))) # It seems the weights are individualized for this updating, maybe this does not work?
+    g_nmda = tau_nmda * dt * (-g_nmda + g_ampa)
+    g_exc = dt * (alpha*g_ampa + (1-alpha)*g_nmda)
+    g_gaba = dt * (-(g_gaba/tau_gaba)+np.sum(w*S_j))
+    g_a = dt * (-(g_a/tau_a)+delta_a*S_j)
+
+
+    # Update membrane potential
+    U = tau_m * dt * ((V_rest - U) + g_exc*(U_exc-U)+(g_gaba + g_a)*(U_inh - U))
+
+    # Update spiking threshold
+    V_
+    
